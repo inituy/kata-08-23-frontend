cb1732eeb1aa3a1c240c3f19860d7f169616b2d1
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2023-08-17 16:16:53 -0300

    fix navbar wallet

--- components/custom-connect-wallet-button/custom-connect-wallet-button.tsx
+++ components/custom-connect-wallet-button/custom-connect-wallet-button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { Wallet, useWallet } from "@solana/wallet-adapter-react";
+import styles from './custom-connect-wallet-button.module.css';
 
 export default function MintButton() {
   const { wallet } = useWallet();
@@ -8,7 +9,9 @@ export default function MintButton() {
   const truncateString = function (wallet: Wallet) {
     const pubkey = wallet?.adapter.publicKey?.toString();
     if (!pubkey) return;
-    return `${pubkey.substring(0, 4)}...${pubkey.substring(39, 43)}`;
+    return (
+      <p className={styles.text_connected}><span>{`(${pubkey.substring(0, 4)}...${pubkey.substring(39, 43)})`}</span></p>
+    );
   };
 
   const getWalletText = function () {
@@ -34,5 +37,5 @@ export default function MintButton() {
     <React.Fragment>
       {renderMustConnectButton()}
     </React.Fragment>
-  )
-}
+  );
+};
